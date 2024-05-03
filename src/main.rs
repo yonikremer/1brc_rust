91a995f30836e04d308bedd4254d3b05ea6ce9f3
yoni <yoni.kremer@gmail.com>
2024-05-03 15:39:22 +0300

    splitting the file into chunks

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
-use std::{fs};
 use std::cmp::{max, min};
 use std::collections::HashMap;
 use std::fmt::{Display, Formatter};
-use std::io::{BufRead, BufReader};
+use std::fs::File;
 use std::num::ParseIntError;
-use std::ops::{Deref, DerefMut};
-use std::sync::{Arc, Mutex, MutexGuard};
-use threadpool::ThreadPool;
+use std::str;
+use std::str::Utf8Error;
 
 const FILE_PATH: &str = "data/measurements.txt";
 const NUM_THREADS: usize = 16;
@@ -68,11 +66,10 @@ fn decimal_str_to_int<'a>(decimal_str: String) -> Result<i16, ParseIntError>{
 }
 
 
-fn print_results(result_maps: Vec<Arc<Mutex<HashMap<String, CityInfo>>>>) -> (){
+fn print_results(result_maps: Vec<HashMap<String, CityInfo>>) -> (){
     let mut result = HashMap::<String, CityInfo>::new();
-    for curr_map_arc in result_maps.iter(){
-        let arc_clone = curr_map_arc.clone();
-        for (city_name, value) in arc_clone.deref().lock().unwrap().deref().iter(){
+    for curr_map in result_maps.iter(){
+        for (city_name, value) in curr_map.iter(){
             if let Some(result_city_info) = result.get_mut(city_name){
                 result_city_info.merge(value);
             }
@@ -88,38 +85,39 @@ fn print_results(result_maps: Vec<Arc<Mutex<HashMap<String, CityInfo>>>>) -> (){
 }
 
 
-
-fn main() {
-    let file = fs::File::open(FILE_PATH).unwrap();
-    let reader = BufReader::new(file);
-    // Create a vector of mutex-protected hash maps (one for each thread)
-    let result_maps: Vec<Arc<Mutex<HashMap<String, CityInfo>>>> = (0..NUM_THREADS)
-        .map(|_| Arc::new(Mutex::new(HashMap::new())))
-        .collect();
-
-    let pool = ThreadPool::new(NUM_THREADS);
-
-    for (thread_id, line) in reader.lines().enumerate() {
-        let line = line.expect("Error reading line");
-        let result_map_clone = result_maps[thread_id % NUM_THREADS].clone();
-        pool.execute(move || {
-            process_line(&line, &result_map_clone);
-        });
-    }
-
-    print_results(result_maps);
-}
-
-fn process_line(line: &String, map: &Arc<Mutex<HashMap<String, CityInfo>>>) {
+fn process_line(line: &str, map: &mut HashMap<String, CityInfo>) {
     let semicolon_index = line.rfind(";").unwrap();
     let city_name: &str = &line[..semicolon_index];
     let temp_string: &str = &line[semicolon_index + 1..];
     let temp_int: i16 = decimal_str_to_int(temp_string.to_string()).unwrap();
-    let mut guard: MutexGuard<HashMap<String, CityInfo>> = map.deref().lock().expect("An error getting the lock to the map");
-    let map_value: &mut HashMap<String, CityInfo> = guard.deref_mut();
-    if let Some(city_info) = map_value.get_mut(city_name) {
+    if let Some(city_info) = map.get_mut(city_name) {
         city_info.add_measurement(temp_int);
     } else {
-        map_value.insert(String::from(city_name), CityInfo::new(temp_int));
+        map.insert(String::from(city_name), CityInfo::new(temp_int));
     }
+}
+
+
+fn process_chunk(chunk: &[u8]) -> Result<HashMap<String, CityInfo>, Utf8Error>{
+    let mut map: HashMap<String, CityInfo> = HashMap::new();
+    let chunk_string: &str = str::from_utf8(chunk)?;
+    for line in chunk_string.split("\n"){
+        process_line(line, &mut map);
+    }
+    return Ok(map);
+}
+
+
+fn main() {
+    use file_chunker::FileChunker;
+    let file = File::open(FILE_PATH).unwrap();
+    let chunker = FileChunker::new(&file).unwrap();
+    let result_maps = chunker.chunks(1_000_000_000 / NUM_THREADS , Some('\n'))
+        .unwrap()
+        .iter()
+        .map(|chunk| {
+            process_chunk(chunk).unwrap()
+        }).collect();
+
+    print_results(result_maps);
 }
